560a2adf7de0ee1bfbcce61487e43eb2401e4a64
Piranavan <piranavanpiran2000@gmail.com>
2023-12-07 14:59:09 +0530

    add

---
+++ index.js
@@ -0,0 +1,30 @@
+//DATABASE CONNECT
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
+
+
+const routes = require('./routes/routes');
+
+app.use('/api', routes)
+
